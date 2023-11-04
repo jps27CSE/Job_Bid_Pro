22016125fdcd03e53601e7971a16df010f8de7ac
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-11-04 21:45:07 +0600

    Added Login Page Template

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,80 @@
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import LoginAnimation from "../looties/login.json";
+import Lottie from "lottie-react";
+
+const Login = () => {
+  return (
+    <div>
+      <div>
+        <div className="hero min-h-screen ">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left">
+              <Lottie
+                className="mx-auto w-80"
+                animationData={LoginAnimation}
+                loop={true}
+              />
+              <h1 className="text-5xl font-bold ">Login now!</h1>
+
+              <p className="py-6">
+                Welcome back! Please sign in to access your account or post a
+                job. If you are new here, you can create an account quickly and
+                easily to start posting or bidding on jobs. We are here to
+                connect you with job opportunities and make your job search or
+                hiring experience even better.
+              </p>
+            </div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+              <form className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="password"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control mt-6">
+                  <button className="btn btn-primary">Login</button>
+                </div>
+                <p>
+                  Create an Account..{" "}
+                  <Link to="/register">
+                    <span className="cursor-pointer text-blue-400">
+                      Register Now
+                    </span>
+                  </Link>
+                </p>
+              </form>
+              <div className="flex mx-auto p-4">
+                <button className="btn btn-ghost">
+                  <FcGoogle className="text-2xl" />
+                  Continue with Google
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,13 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Home from "../components/Home/Home";
+import Login from "../components/Login/Login";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-     
-      
-    },
-  ]);
-  
-  export default router;
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+export default router;
