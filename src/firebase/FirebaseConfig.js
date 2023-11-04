77614f1379b0c2fa6f3254299b76dbb2264c8f3c
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-11-04 20:38:37 +0600

    Firebase Config Added

--- src/firebase/FirebaseConfig.js
+++ src/firebase/FirebaseConfig.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCZMDnZF7oYUmoV0rop-XlPxaBBP_zK0gA",
-  authDomain: "jobbidpro.firebaseapp.com",
-  projectId: "jobbidpro",
-  storageBucket: "jobbidpro.appspot.com",
-  messagingSenderId: "360740496938",
-  appId: "1:360740496938:web:a44b9cf4d38f72aff9c553"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSASING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
