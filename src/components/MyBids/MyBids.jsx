de8aba4977d7aa52eb16d8b16f0d403cf58d70d6
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-11-05 15:42:40 +0600

    My Bids Fake Data Ready Template

--- src/components/MyBids/MyBids.jsx
+++ src/components/MyBids/MyBids.jsx
@@ -1,4 +1,33 @@
+import { useState } from "react";
+
 const MyBids = () => {
+  const [bids, setBids] = useState([
+    {
+      jobTitle: "Job Title 1",
+      email: "user1@example.com",
+      deadline: "2023-12-31",
+      status: "pending",
+    },
+    {
+      jobTitle: "Job Title 2",
+      email: "user2@example.com",
+      deadline: "2023-11-30",
+      status: "in-progress",
+    },
+    {
+      jobTitle: "Job Title 3",
+      email: "user3@example.com",
+      deadline: "2023-11-15",
+      status: "canceled",
+    },
+  ]);
+
+  const handleComplete = (index) => {
+    const updatedBids = [...bids];
+    updatedBids[index].status = "complete";
+    setBids(updatedBids);
+  };
+
   return (
     <div>
       <div className="overflow-x-auto">
@@ -6,133 +35,32 @@ const MyBids = () => {
           {/* head */}
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-              <th></th>
+              <th>Job Title</th>
+              <th>Email</th>
+              <th>Deadline</th>
+              <th>Status</th>
+              <th>Complete</th>
             </tr>
           </thead>
           <tbody>
-            {/* row 1 */}
-            <tr>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-2@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Hart Hagerty</div>
-                    <div className="text-sm opacity-50">United States</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Zemlak, Daniel and Leannon
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Desktop Support Technician
-                </span>
-              </td>
-              <td>Purple</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            {/* row 2 */}
-            <tr>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-3@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Brice Swyre</div>
-                    <div className="text-sm opacity-50">China</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Carroll Group
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Tax Accountant
-                </span>
-              </td>
-              <td>Red</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            {/* row 3 */}
-            <tr>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-4@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Marjy Ferencz</div>
-                    <div className="text-sm opacity-50">Russia</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Rowe-Schoen
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Office Assistant I
-                </span>
-              </td>
-              <td>Crimson</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            {/* row 4 */}
-            <tr>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-5@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Yancy Tear</div>
-                    <div className="text-sm opacity-50">Brazil</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Wyman-Ledner
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Community Outreach Specialist
-                </span>
-              </td>
-              <td>Indigo</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
+            {bids.map((bid, index) => (
+              <tr key={index}>
+                <td>{bid.jobTitle}</td>
+                <td>{bid.email}</td>
+                <td>{bid.deadline}</td>
+                <td>{bid.status}</td>
+                <td>
+                  {bid.status === "in-progress" && (
+                    <button
+                      className="btn btn-primary btn-xs"
+                      onClick={() => handleComplete(index)}
+                    >
+                      Complete
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
