6e49e541937749bbf1d95927e111d1ab95ee9bcf
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-11-06 14:30:54 +0600

    banner fix

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -14,17 +14,21 @@ const Banner = () => {
       >
         <div className="hero-overlay bg-opacity-80"></div>
         <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-4xl font-bold">
-              Welcome to <span className="text-lime-400">JobBidPro</span> Your
-              Ultimate Job Marketplace!
-            </h1>
+          <div className=" flex flex-col lg:flex-row">
+            <div className="max-w-md">
+              <h1 className="mb-5 mt-0 lg:mt-24 text-4xl font-bold">
+                Welcome to <span className="text-lime-400">JobBidPro</span> Your
+                Ultimate Job Marketplace!
+              </h1>
+            </div>
+            <div>
+              <Lottie
+                className="mx-auto w-80"
+                animationData={BannerAnimation}
+                loop={true}
+              />
+            </div>
           </div>
-          <Lottie
-            className="mx-auto w-80"
-            animationData={BannerAnimation}
-            loop={true}
-          />
         </div>
       </div>
     </div>
